356070e47568af259c6e87e75e6244ad707f8d78
Pankaj chejara <pankajchejara23@gmail.com>
2024-05-10 16:39:18 +0300

    udpate

--- app.py
+++ app.py
@@ -1614,3 +1614,6 @@ def display_value(n_clicks, value, lg_metric, sm_metric, sm_outcome, lg_outcome)
     figure.update_layout(height=1100,
                          font_size=14)
     return figure
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
